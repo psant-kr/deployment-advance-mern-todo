3e2e9310e90f7bd574534d294b9343528bcec5c7
prashant kumar <prashantdoa1996@gmail.com>
2023-09-28 03:42:34 +0530

    verson 1

---
+++ server/index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
+import Connection from './database/db.js';
+import Routes from './routes/route.js';
+
+const PORT = process.env.PORT || 8000
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/', Routes);
+
+app.get("/",(req,res)=>{
+    res.json("HELLO...");
+})
+
+// const PORT = 8000;
+
+Connection();
+
+app.listen(PORT, () => console.log(`Your server is running sucessfully on PORT ${PORT}`));
